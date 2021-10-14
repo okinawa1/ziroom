1b46631f1c2083581552f7fec42fa6f1f41f50b3
袁华健 <yuanhuajian@bixin.cn>
2021-10-14 10:04:18 +0800

    fix:调整时间

--- src/main/kotlin/AppleNotify.kt
+++ src/main/kotlin/AppleNotify.kt
@@ -11,21 +11,25 @@ fun main() {
     val location = URLEncoder.encode("上海 上海")
     val phone = "MLTE3CH" //256g iphone 13 pro 远空蓝
     while (true) {
-        val resp =
-            "https://www.apple.com.cn/shop/fulfillment-messages?pl=true&parts.0=$phone/A&location=$location".httpGet()
-        val string = resp.body()?.string()
-        println(string)
-        val applePhoneResp = gson.fromJson(string, ApplePhoneResp::class.java)
-        val availableStore =
-            applePhoneResp.body.content.pickupMessage.stores
-                .map { store -> store.partsAvailability.phone }
-                .filterNot { p -> p.storePickupQuote.contains(Regex(".*苏州.*|.*无锡.*|.*杭州.*|.*西湖.*|.*天一.*")) }
-                .filter { p -> p.pickupDisplay == "available" }
+        val applePhoneResp: ApplePhoneResp = try {
+            val resp =
+                "https://www.apple.com.cn/shop/fulfillment-messages?pl=true&parts.0=$phone/A&location=$location".httpGet()
+            val string = resp.body()?.string()
+            println(string)
+            gson.fromJson(string, ApplePhoneResp::class.java)
+        } catch (e: Exception) {
+            println(e.printStackTrace())
+            continue
+        }
+        val availableStore = applePhoneResp.body.content.pickupMessage.stores
+            .map { store -> store.partsAvailability.phone }
+            .filterNot { p -> p.storePickupQuote.contains(Regex(".*苏州.*|.*无锡.*|.*杭州.*|.*西湖.*|.*天一.*")) }
+            .filter { p -> p.pickupDisplay == "available" }
         if (availableStore.isNotEmpty()) {
             println(availableStore)
             WechatSender().sendMsg(availableStore.joinToString(",") { s -> s.storePickupQuote })
         }
-        sleep(500)
+        sleep(800)
     }
 }
 
